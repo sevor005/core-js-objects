b90164e7bce4049d67802e60390bfb75387e6c16
Sergey Vorontsov <sevor005@yandex.ru>
2025-02-25 20:41:18 +0700

    feat: core-js-objects path 2.

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -79,8 +79,8 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -94,8 +94,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return !Object.keys(obj).length;
 }
 
 /**
@@ -114,8 +114,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -128,8 +128,27 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  if (!Object.keys(lettersObject).length) return '';
+
+  const maxLength = Math.max(
+    ...Object.values(lettersObject).map((num) => Math.max(...num))
+  );
+  let result = '';
+
+  [...Array(maxLength + 1).keys()].forEach((i) => {
+    result += Object.entries(lettersObject)
+      .reduce((acc, [key, values]) => {
+        if (values.includes(i)) {
+          acc.push(key);
+        }
+
+        return acc;
+      }, [])
+      .join('');
+  });
+
+  return result;
 }
 
 /**
@@ -146,8 +165,18 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let money = 0;
+
+  return queue.every((item) => {
+    if (item - 25 > money) {
+      return false;
+    }
+
+    money += item;
+
+    return true;
+  });
 }
 
 /**
